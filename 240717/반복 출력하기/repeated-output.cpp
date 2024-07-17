3f7b032285a6762d38e8d8bedde4923301371f1c
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-07-17 15:28:34 +0900

    Title: 반복 출력하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void print(int n) {
+    while(n--){
+        cout << "12345^&*()_" << endl;
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
