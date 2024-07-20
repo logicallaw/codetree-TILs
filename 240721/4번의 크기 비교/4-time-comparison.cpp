e66234de0a8c214e4c35a879ca475804bb08dbf8
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-07-21 01:26:13 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240721/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c,d,e;
+    cin >> a >> b >> c >> d >> e;
+    cout << ((a > b) ? 1 : 0) << endl;
+    cout << ((a > c) ? 1 : 0) << endl;
+    cout << ((a > d) ? 1 : 0) << endl;
+    cout << ((a > e) ? 1 : 0) << endl;
+    return 0;
+}
