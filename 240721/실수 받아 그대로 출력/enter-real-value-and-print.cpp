bd66792fc0946becd91b0443ae3ecf6ae36dcabf
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-07-21 00:24:54 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240721/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double d;
+    cin >> d;
+    cout << setprecision(4);
+    cout << d;
+    return 0;
+}
