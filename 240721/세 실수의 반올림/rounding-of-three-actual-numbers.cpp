35fef81f844dd0a4d83fc19afd6569686da78b75
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-07-21 01:32:09 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240721/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a, b, c;
+    cin >> a >> b >> c;
+    printf("%.3f\n%.3f\n%.3f", a, b, c);
+    return 0;
+}
