08d9b56b18e64e6fbdf4098aeee2ffc87d6c6e29
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-07-21 01:04:56 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240721/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int score;
+    cin >> score;
+    string result = (score == 100) ? "pass" : "failure";
+    cout << result;
+    return 0;
+}
