203e1a90b6793c8b89059c5e5d82a302c84cfb1f
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-07-24 15:34:42 +0900

    Title: 자리 바꾸기 2, Time: 492ms, Memory: 40MB, Status: Passed - CodeTree

---
+++ 240724/자리 바꾸기 2/changing-seats-2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+void swap(int* x, int* y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+int main() {
+    int n, m;
+    cin >> n >> m;
+    unordered_map<int, unordered_set<int>*> map;
+    int* array = new int[n + 1];
+    array[0] = 0;
+    for(int i{ 1 }; i <= n; i++) {
+        unordered_set<int>* ptr = new unordered_set<int>;
+        ptr->insert(i);
+        map.insert({i, ptr});
+        array[i] = i;
+    }
+    int** matrix = new int*[m];
+    for(int i{ 0 }; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        matrix[i] = new int[2]{a, b};
+    }
+
+    for(int i{ 1 }; i <= 3; i++) {
+        for(int j { 0 }; j < m; j++) {
+            int ai = matrix[j][0];
+            int bi = matrix[j][1];
+            int x = array[ai];
+            int y = array[bi];
+//            cout << "before:" << array[ai] << " " << array[bi] << endl;
+            if(map[x]->find(bi) == map[x]->end()) {
+//                cout << "map[" << x << "] new:" << bi  << endl;
+                map[x]->insert(bi);
+            }
+            if(map[y]->find(ai) == map[y]->end()) {
+//                cout << "map[" << y << "] new:" << ai << endl;
+                map[y]->insert(ai);
+            }
+            swap(array[ai], array[bi]);
+//            cout << "after:" << array[ai] << " " << array[bi] << endl;
+        }
+//        cout << "HI" << endl;
+    }
+    for(int i{ 1 }; i <= n; i++) {
+        unordered_set<int>* ptr = map[i];
+        cout << ptr->size() << endl;
+    }
+    for(int i{ 0 }; i < m; i++) {
+        delete matrix[i];
+    }
+    delete[] matrix;
+    delete[] array;
+    return 0;
+}
