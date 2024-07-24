e9dec02b657278e7f79ef06e54da45de8f8454d7
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-07-24 15:40:14 +0900

    Title: 자리 바꾸기 2, Time: 529ms, Memory: 36MB, Status: Passed - CodeTree

--- 240724/자리 바꾸기 2/changing-seats-2.cpp
+++ 240724/자리 바꾸기 2/changing-seats-2.cpp
@@ -2,22 +2,20 @@
 #include <unordered_set>
 #include <unordered_map>
 using namespace std;
-void swap(int* x, int* y){
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
+//void swap(int* x, int* y){
+//    int temp = *x;
+//    *x = *y;
+//    *y = temp;
+//}
 int main() {
     int n, m;
     cin >> n >> m;
-    unordered_set<int> map[n + 1];
+    unordered_set<int> set[n + 1];
     int* array = new int[n + 1];
     array[0] = 0;
     for(int i{ 1 }; i <= n; i++) {
-        unordered_set<int>* ptr = new unordered_set<int>;
-        ptr->insert(i);
-        map[i].insert(i);
         array[i] = i;
+        set[i].insert(i);
     }
     int** matrix = new int*[m];
     for(int i{ 0 }; i < m; i++) {
@@ -32,23 +30,17 @@ int main() {
             int bi = matrix[j][1];
             int x = array[ai];
             int y = array[bi];
-//            cout << "before:" << array[ai] << " " << array[bi] << endl;
-            if(map[x].find(bi) == map[x].end()) {
-//                cout << "map[" << x << "] new:" << bi  << endl;
-                map[x].insert(bi);
+            if(set[x].find(bi) == set[x].end()) {
+                set[x].insert(bi);
             }
-            if(map[y].find(ai) == map[y].end()) {
-//                cout << "map[" << y << "] new:" << ai << endl;
-                map[y].insert(ai);
+            if(set[y].find(ai) == set[y].end()) {
+                set[y].insert(ai);
             }
             swap(array[ai], array[bi]);
-//            cout << "after:" << array[ai] << " " << array[bi] << endl;
         }
-//        cout << "HI" << endl;
     }
     for(int i{ 1 }; i <= n; i++) {
-        unordered_set<int> ptr = map[i];
-        cout << ptr.size() << endl;
+        cout << set[i].size() << endl;
     }
     for(int i{ 0 }; i < m; i++) {
         delete matrix[i];
