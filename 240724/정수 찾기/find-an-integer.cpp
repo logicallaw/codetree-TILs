c91f17e2c1607090adb1d70016a954baedc7d49a
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-07-24 14:51:10 +0900

    Title: 정수 찾기, Time: 205ms, Memory: 3MB, Status: Passed - CodeTree

---
+++ 240724/정수 찾기/find-an-integer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    unordered_set<int> set;
+    while(n--) {
+        int x;
+        cin >> x;
+        if(set.find(x) == set.end()) {
+            set.insert(x);
+        }
+    }
+    int m;
+    cin >> m;
+    while(m--) {
+        int x;
+        cin >> x;
+        if(set.find(x) != set.end()) {
+            cout << 1 << endl;
+        } else {
+            cout << 0 << endl;
+        }
+    }
+    return 0;
+}
