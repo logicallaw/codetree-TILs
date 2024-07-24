545a5f45f504b55fa83e27e76f6cdac1900dd137
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-07-24 15:38:01 +0900

    Title: 자리 바꾸기 2, Time: 630ms, Memory: 51MB, Status: Passed - CodeTree

--- 240724/자리 바꾸기 2/changing-seats-2.cpp
+++ 240724/자리 바꾸기 2/changing-seats-2.cpp
@@ -10,13 +10,13 @@ void swap(int* x, int* y){
 int main() {
     int n, m;
     cin >> n >> m;
-    unordered_map<int, unordered_set<int>*> map;
+    unordered_set<int> map[n + 1];
     int* array = new int[n + 1];
     array[0] = 0;
     for(int i{ 1 }; i <= n; i++) {
         unordered_set<int>* ptr = new unordered_set<int>;
         ptr->insert(i);
-        map.insert({i, ptr});
+        map[i].insert(i);
         array[i] = i;
     }
     int** matrix = new int*[m];
@@ -33,13 +33,13 @@ int main() {
             int x = array[ai];
             int y = array[bi];
 //            cout << "before:" << array[ai] << " " << array[bi] << endl;
-            if(map[x]->find(bi) == map[x]->end()) {
+            if(map[x].find(bi) == map[x].end()) {
 //                cout << "map[" << x << "] new:" << bi  << endl;
-                map[x]->insert(bi);
+                map[x].insert(bi);
             }
-            if(map[y]->find(ai) == map[y]->end()) {
+            if(map[y].find(ai) == map[y].end()) {
 //                cout << "map[" << y << "] new:" << ai << endl;
-                map[y]->insert(ai);
+                map[y].insert(ai);
             }
             swap(array[ai], array[bi]);
 //            cout << "after:" << array[ai] << " " << array[bi] << endl;
@@ -47,8 +47,8 @@ int main() {
 //        cout << "HI" << endl;
     }
     for(int i{ 1 }; i <= n; i++) {
-        unordered_set<int>* ptr = map[i];
-        cout << ptr->size() << endl;
+        unordered_set<int> ptr = map[i];
+        cout << ptr.size() << endl;
     }
     for(int i{ 0 }; i < m; i++) {
         delete matrix[i];
