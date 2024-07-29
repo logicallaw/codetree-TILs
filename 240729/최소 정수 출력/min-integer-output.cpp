15aa172c221e24027f72c4ec43f339ee0dcb7c8b
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-07-29 21:19:41 +0900

    Title: 최소 정수 출력, Time: 46ms, Memory: 1MB, Status: Passed - CodeTree

--- 240729/최소 정수 출력/min-integer-output.cpp
+++ 240729/최소 정수 출력/min-integer-output.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <queue>
+#include <vector>
+#include <functional>
 using namespace std;
 int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n;
     cin >> n;
-    priority_queue<int> pq;
+    priority_queue<int,vector<int>, greater<int>> pq;
     for(int i{ 0 }; i < n; i++) {
         int x;
         cin >> x;
         if(x == 0) {
             if(pq.empty()) {
-                cout << 0 << endl;
+                printf("%d\n", 0);
             } else {
                 int min = pq.top();
-                cout << -min << endl;
+                printf("%d\n", min);
                 pq.pop();
             }
         } else {
-            pq.push(-x);
+            pq.push(x);
         }
     }
 }
