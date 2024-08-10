43f233eff31b3eca45adf0cb78b19ba9fc75db35
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-08-10 18:21:37 +0900

    Title: 그래프 탐색, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240810/그래프 탐색/graph-traversal.cpp
+++ 240810/그래프 탐색/graph-traversal.cpp
@@ -5,14 +5,18 @@ using namespace std;
 vector<int> edges[1001] {};
 bool visited[1001] {};
 
-int dfs(int vertex, int cnt) {
+int result = 0;
+
+void dfs(int vertex, int cnt) {
     visited[vertex] = true;
     for(const int child : edges[vertex]) {
         if(!visited[child]) {
-            return dfs(child, cnt + 1);
+            dfs(child, cnt + 1);
         }
     }
-    return cnt;
+    if (result < cnt) {
+        result = cnt;
+    }
 }
 
 int main() {
@@ -25,7 +29,7 @@ int main() {
         edges[x].push_back(y);
         edges[y].push_back(x);
     }
-    int result = dfs(1, 0);
+    dfs(1, 0);
     cout << result;
     return 0;
 }
