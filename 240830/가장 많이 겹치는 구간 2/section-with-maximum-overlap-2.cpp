2e5a7fc0f8421c2168f605c57a49d3e54477c824
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-08-30 16:33:38 +0900

    Title: 가장 많이 겹치는 구간 2, Time: 17ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240830/가장 많이 겹치는 구간 2/section-with-maximum-overlap-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n;
+    cin >> n;
+    long long checked[100001] {};
+    for(int i{ 0 }; i < n; i++) {
+        long long x1, x2;
+        cin >> x1 >> x2;
+        checked[x1]++;
+        checked[x2]--;
+    }
+    int sum_val = 0;
+    int max = 1;
+    for(long long ele : checked) {
+        sum_val += ele;
+        max = (max < sum_val) ? sum_val : max;
+    }
+    cout << max;
+}
