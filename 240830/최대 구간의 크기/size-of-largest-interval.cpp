36ec68e3888a3ee7f1b6b7b701c7384f5dcab526
logicallaw <93714449+logicallaw@users.noreply.github.com>
2024-08-30 16:59:37 +0900

    Title: 최대 구간의 크기, Time: 160ms, Memory: 4MB, Status: Passed - CodeTree

---
+++ 240830/최대 구간의 크기/size-of-largest-interval.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(void) {
+    int n;
+    cin >> n;
+    vector<tuple<long long, int, int>> v;
+    for(int i{ 0 }; i < n; i++) {
+        long long x1, x2;
+        cin >> x1 >> x2;
+        v.push_back(make_tuple(x1, +1, i));
+        v.push_back(make_tuple(x2, -1, i));
+    }
+    long long result = 0;
+    unordered_set<int> s;
+
+    long long start = 1, end = 1;
+
+    sort(v.begin(), v.end()); //오름차순
+    for(auto p : v) {
+        int w = get<1>(p);
+        if(w == 1) {
+            if(s.size() == 0) {
+                start = get<0>(p);
+            }
+            s.insert({get<2>(p)});
+        } else { // w == -1
+            s.erase(get<2>(p));
+            if(s.size() == 0) {
+                end = get<0>(p);
+                long long diff = end - start;
+                result = (result < diff) ? diff : result;
+            }
+        }
+    }
+    cout << result;
+    return 0;
+}
