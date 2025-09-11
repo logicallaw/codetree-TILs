ca68b4e6515495e93c9af58d1b12cfdacaf2abab
Junho Kim <93714449+logicallaw@users.noreply.github.com>
2025-09-11 20:16:46 +0900

    Title: 각 정점까지의 최단 경로 2, Time: 27ms, Memory: 0MB, Status: Passed - Codetree

--- 250911/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.cpp
+++ 250911/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.cpp
@@ -43,7 +43,7 @@ int main() {
     for (int i = 1; i <=n; i++) {
         for (int j = 1; j <=n; j++) {
             if (d[i][j] == UNSEEN) {
-                cout << 0 << " ";
+                cout << -1 << " ";
                 continue;
             }
             cout << d[i][j] << " ";
