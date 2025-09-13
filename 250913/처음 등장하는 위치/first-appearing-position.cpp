70a6dc419ce9c7d88ad4f076f5ead415594484d7
Junho Kim <93714449+logicallaw@users.noreply.github.com>
2025-09-13 16:57:55 +0900

    Title: 처음 등장하는 위치, Time: 32ms, Memory: 2MB, Status: Wrong Answer - Codetree

---
+++ 250913/처음 등장하는 위치/first-appearing-position.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+
+    map<int, int> m;
+    int cnt = 0;
+    while (N--) {
+        int x;
+        cin >> x;
+
+        if (m.find(x) == m.end()) {
+            m.insert({x, ++cnt});
+        }
+    }
+
+    map<int, int>::iterator it;
+    for (it = m.begin(); it != m.end(); it++) {
+        cout << it->first << " " << it->second << "\n";
+    }
+}
